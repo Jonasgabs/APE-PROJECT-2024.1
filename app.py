9ad799bfa14c64bd36c69c65fd01f54c540b1c08
gabreisndev <gabrielsn300@gmail.com>
2024-09-13 08:02:58 -0300

    logo e código

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+from PIL import Image
 from customtkinter import *
 
 janela = CTk()
 janela.geometry("400x500")
 
+
+image = Image.open('img/Tribunal_Superior_Eleitoral.png')
+img = CTkImage(light_image = image, dark_image = image, size = (275, 75))
+logo_label = CTkLabel(master = janela, image = img)
+logo_label.pack(pady = (20, 10))
 
 janela.mainloop()
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import pandas as pd
 
+class Main():
+
+    def valores_unicos(self):
+        self.df = pd.read_csv('consulta_cand_2024_PB.csv', encoding='ISO-8859-1', sep=';', on_bad_lines='skip')
+        #print("Colunas disponíveis no DataFrame:", self.df.columns)
+        self.valoresunicos = self.df['SG_UE'].unique()
+        print(self.valoresunicos)
+
+
+
+    def main(self):
+        self.valores_unicos()
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.main()
