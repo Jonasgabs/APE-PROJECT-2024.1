b063e165822cc9ddf2724fade801fd60d4e295a8
Jonas Sarmento <jonas.sarmento@academico.ifpb.edu.br>
2024-10-03 14:08:32 -0300

    Update pages.py

--- pages.py
+++ pages.py
@@ -341,7 +341,6 @@ def generate_html_candidatos(candidatos):
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Seu Candidato</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
->
     </head>
     <body class="bg-secondary text-black">
         <div class="container">
